1aa5698ac10a4ff499daa5da34812beaf250bf49
Mr-cody-77 <sonu77mahata@gmail.com>
2025-10-18 10:16:43 +0530

    db changed

--- backend/settings.py
+++ backend/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+import dj_database_url
+load_dotenv() 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
